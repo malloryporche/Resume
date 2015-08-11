e1eebc9d304e18700dee7091fa9b1cab497423cf
MalloryPorche <malloryburke@gmail.com>
2015-08-11 12:46:39 -0700

    Initial commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,59 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	'name': 'Mallory',
+	'role': 'Front End Web Developer',
+	'welcomeMessage': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione non velit eos, dicta illum rerum? Voluptatum quae eum, autem saepe, recusandae quod dolorum aliquam temporibus velit corrupti accusamus eius adipisci?',
+	'mobileContact': '323.294.1774',
+	'emailAddress': 'malloryburke@gmail.com',
+	'githubUsername': ' @malloryporche',
+	'twitterHandle': 'missmalloree',
+	'location': '5060 West 58th Place, Los Angeles, California 90056',
+	'skills': ['HTML5', 'CSS3', 'jQuery', 'Javascript'];
+};
+
+var work = {
+	'jobs': [
+		{
+			'employer': 'Powered Office, Inc.',
+			'title': 'Tier2 Account / Project Manager',
+			'location': 'Hollywood, California',
+			'datesWorked': 'September - October 2014',
+			'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur accusantium, animi, quis quasi in tenetur alias dignissimos quas dicta explicabo voluptate nam distinctio quibusdam enim eius iste iusto dolorem, hic!';
+		},
+		{
+			'employer': 'AEI Consultants, Inc.',
+			'title': 'Administrative Assitant',
+			'location': 'Culver City, California',
+			'datesWorked': 'October 2012 - July 2013',
+			'description': 'Assisted Vice President of national firm on multiple pet projects to aid in the assistance and efficacy of the Southern California office.'
+		},
+		{
+			'employer': 'Phillips Graduate Institute',
+			'title': 'Assistant Manager, Student Store',
+			'location': 'Encino, Califorina',
+			'datesWorked': 'January 2010 - July 2012',
+			'description': 'Point-of-sales displays, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum illum eum nemo, dolor, recusandae officiis alias tempora. Magnam maxime, ex dolore quo molestias culpa et libero vero, velit sed assumenda.';
+		};
+	]
+};
+	
+
+var education = {
+	"schools": [
+		{
+			'name': 'University of Southern California',
+			'location': 'Los Angeles, California',
+			'degree': 'IP',
+			'major': ['Political Science', 'Race, Ethnicity, and Politics',
+			'dates': '2004 - 2010',
+			'url': 'www.usc.edu';
+		},
+		{
+			'courseTitle': 'Javascript Basics',
+			'courseURL': 'www.udacity.com',
+			'school': 'www.udacity.com',
+			'datesAttended': 'August 2015',
+			'url to course': ''
+			'date': 'August 2015';
+		};
+	]
+};
